4ed9d1a3ab4e19a44163729dab5a5a0ef130cad9
Chandradeo Kumar <kumarchandradeo40@gmail.com>
2024-11-07 14:32:01 +0530

    Initial commit

---
+++ myapp/src/Components/Header.js
@@ -0,0 +1,59 @@
+// {function Header() {
+//     return ( <div>Abhi</div>);
+// }
+
+//   function Header2() {
+//     return ( <div>Abhi2</div>);
+// }
+//   function Main() {
+//     return ( <div> Abhi3</div>);
+// }
+//   function Header3() {
+//     return ( <div> Abhi4</div>);
+// }
+  
+
+//   export {Header, Header2, Header3}; 
+//   export default Main;
+
+
+// import React from 'react';
+// import "./Header.css";  
+
+// const Header = () => {
+//    return (
+//     <nav>How are you</nav>
+//   )
+
+// return(
+//     <nav style={{
+//         // backgroundColor:"blueviolet",
+//         border: "5px solid red"
+
+//     }}>How Are You</nav>
+//  )
+
+//  const headerstyle={
+//     border: "5px solid red",
+//     background:"red",
+// }
+//   return (
+//     <nav style={headerstyle}>How are you</nav>
+//   )
+// }
+
+// export default Header;
+// }
+
+
+
+import React from 'react';
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <nav>Header</nav>
+  )
+}
+
+export default Header
